30a97c44e01d6f7e2101dc481eb8a0c990e45b5c
sht02048 <sht02048@gmail.com>
2024-11-12 21:42:41 +0900

    💄 Anonymous 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'anonymous': ['"Anonymous Pro"', 'monospace'],
+      }
+    },
   },
   plugins: [],
 }

353abd0efbb85d780a42e048ad5cb692181cb903
sht02048 <sht02048@gmail.com>
2024-11-14 10:10:14 +0900

    ✨ Curve 컴포넌트

--- src/components/Header/ToolBar/index.tsx
+++ src/components/Header/ToolBar/index.tsx
@@ -10,9 +10,9 @@ export default function ToolBar() {
       <div className="flex items-center justify-center w-auto gap-2">
         <Tool toolType={TOOL_TYPE.SELECT} src="/src/assets/grab.svg" />
         <Tool toolType={TOOL_TYPE.LINE} src="/src/assets/linear.svg" />
+        <Tool toolType={TOOL_TYPE.CURVE} src="/src/assets/curve.svg" />
         <Tool toolType={TOOL_TYPE.CIRCLE} src="/src/assets/circle.svg" />
         <Tool toolType={TOOL_TYPE.RECTANGLE} src="/src/assets/square.svg" />
-        <Tool toolType={TOOL_TYPE.TRIANGLE} src="/src/assets/triangle.svg" />
         <Color />
         <StrokeWidth />
       </div>
